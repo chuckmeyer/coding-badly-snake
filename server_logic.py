dea53dff1b1b46aa70a04032794d8de2f8d24f0c
Chuck Meyer <chuck.meyer@algolia.com>
2021-12-09 16:57:06 -0500

    Added search for food. Should be fine.

--- server_logic.py
+++ server_logic.py
@@ -1,5 +1,11 @@
+from algoliasearch.search_client import SearchClient
 import random
 from typing import List, Dict
+
+ALGOLIA_APP_ID = environ.get('ALGOLIA_APP_ID')
+ALGOLIA_API_KEY = environ.get('ALGOLIA_API_KEY')
+
+client = SearchClient.create(ALGOLIA_APP_ID, ALGOLIA_API_KEY)
 
 """
 This file can be a nice home for your move logic, and to write helper functions.
@@ -7,6 +13,36 @@ This file can be a nice home for your move logic, and to write helper functions.
 We have started this for you, with a function to help remove the 'neck' direction
 from the list of possible moves!
 """
+
+def apple_vision(size: int, my_head: Dict[str, int], board: Dict[str, int]) -> str:
+    vision = ''
+    x_range = range(my_head['x'] - size//2, my_head['x'] + size//2 + 1)
+    y_range = range(my_head['y'] - size//2, my_head['y'] + size//2 + 1)
+
+    for y in y_range:
+        for x in x_range:
+            space = "S"
+            if my_head["x"] == x and my_head["y"] == y:
+                space = "H"
+            else:
+                for apple in board["food"]:
+                    if x == apple["x"] and y == apple["y"]:
+                        space = "O"
+            vision += space
+    return vision
+
+
+def find_food(size: int, vision: str, possible_moves: List[str]) -> List[str]:
+    best_moves = []
+    index_name = 'boards-apples-' + str(size) + 'x' + str(size)
+    index = client.init_index(index_name)
+    results = index.search(vision)
+    if results['hits']:
+        for move in results['hits']['0']['best_move']:
+            if move in possible_moves:
+                best_moves.append(move)
+    return best_moves
+
 
 def remove_move(move: str, possible_moves: List[str]) -> List[str]:
     if move in possible_moves:
@@ -74,6 +110,7 @@ def choose_move(data: dict) -> str:
     """
     my_head = data["you"]["head"]  # A dictionary of x/y coordinates like {"x": 0, "y": 0}
     my_body = data["you"]["body"]  # A list of x/y coordinate dictionaries like [ {"x": 0, "y": 0}, {"x": 1, "y": 0}, {"x": 2, "y": 0} ]
+    my_vision = apple_vision(5, my_head, data["board"])
 
     # TODO: uncomment the lines below so you can see what this data looks like in your output!
     # print(f"~~~ Turn: {data['turn']}  Game Mode: {data['game']['ruleset']['name']} ~~~")
@@ -85,22 +122,29 @@ def choose_move(data: dict) -> str:
 
     # Don't allow your Battlesnake to move back in on it's own neck
     possible_moves = avoid_my_neck(my_head, my_body, possible_moves)
-    possible_moves = avoid_walls(my_head, data["board"], possible_moves)
-    
+
     # TODO: Using information from 'data', find the edges of the board and don't let your Battlesnake move beyond them
     # board_height = ?
     # board_width = ?
+    possible_moves = avoid_walls(my_head, data["board"], possible_moves)
 
     # TODO Using information from 'data', don't let your Battlesnake pick a move that would hit its own body
 
     # TODO: Using information from 'data', don't let your Battlesnake pick a move that would collide with another Battlesnake
 
     # TODO: Using information from 'data', make your Battlesnake move towards a piece of food on the board
+    best_moves = find_food(5, my_vision, possible_moves)
 
     # Choose a random direction from the remaining possible_moves to move in, and then return that move
-    move = random.choice(possible_moves)
+    if possible_moves:
+        for best_move in best_moves:
+            if best_moves in possible_moves:
+                move = best_move
+                print(f"{data['game']['id']} MOVE {data['turn']}: {move} picked as best move {possible_moves}")
+    if not move:
+        move = random.choice(possible_moves)
+        print(f"{data['game']['id']} MOVE {data['turn']}: {move} picked randomly from all valid options in {possible_moves}")
+    
     # TODO: Explore new strategies for picking a move that are better than random
-
-    print(f"{data['game']['id']} MOVE {data['turn']}: {move} picked from all valid options in {possible_moves}")
 
     return move
